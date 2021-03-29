8c2187c500a28e1c44ba6bf00dbcddc8f8c580b0
iromashko <iromashko@me.com>
2021-03-29 22:18:52 +0300

    add markup

--- libs/auth/src/lib/components/login-form/login-form.component.ts
+++ libs/auth/src/lib/components/login-form/login-form.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { Authenticate } from '@enterprise-angular/data-models';
 
 @Component({
   selector: 'enterprise-angular-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {}
+export class LoginFormComponent {
+  @Output() submit = new EventEmitter<Authenticate>();
+
+  login(authenticate: Authenticate) {
+    this.submit.emit(authenticate);
+  }
+}
--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'enterprise-angular-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
   login(authenticate: any) {
